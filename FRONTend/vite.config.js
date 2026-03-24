400c47c02b6da883048083b48506b097e78036c6
Srishanth3524 <srishanthreddy3524@gmail.com>
2026-03-24 12:43:22 +0530

    Update vite.config.js

--- FRONTend/vite.config.js
+++ FRONTend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://medicine-2-zc51.onrender.com',
         changeOrigin: true
       }
     }
